31a08bc77861cf2294f38368840a0fe263294fbe
Phossi <53015987+Phossi@users.noreply.github.com>
2022-08-05 18:56:27 +0200

    feat: schemas (#4)
    * feat: schemas
    
    * Update test.schema.ts
    
    Co-authored-by: Nissl <43213993+Nis5l@users.noreply.github.com>

---
+++ server/src/schemas/request.schema.ts
@@ -0,0 +1,31 @@
+import { Schema, Model } from 'mongoose';
+import { HttpMethods, Request } from '../tests/interfaces';
+
+export function createRestEndpointSchema(): Schema<Request, Model<Request>, Request> {
+  return new Schema({
+    url: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    method: {
+      enum: HttpMethods,
+      required: true,
+    },
+    headers: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+    payload: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+    response: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+    statusCode: {
+      type: Schema.Types.Number,
+      required: true,
+    },
+  });
+}
